e654daa00613f4524162fb7a89d37996e7a4c668
Urtats Berrocal <urtatsberrocal@gmail.com>
2023-02-18 17:55:36 +0100

    Sortuta getArrivals eta getDepartingCities

--- src/main/java/dataAccess/Fligth_objectdbAccess.java
+++ src/main/java/dataAccess/Fligth_objectdbAccess.java
@@ -1,8 +1,12 @@
 package dataAccess;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import domain.Flight;
 
@@ -39,11 +43,32 @@ public class Fligth_objectdbAccess {
 		close();
 	}
 	
-	public List<String> getDepartingCities()
+	public List<String> getDepartingCities(String city)
 	{
 		emf = Persistence.createEntityManagerFactory(DBName);
 		db = emf.createEntityManager();
 		System.out.println("Database ON");
+		
+		TypedQuery<String> query = db.createQuery("SELECT DISTINCT f.departingCity FROM Flight f", String.class);
+		query.setParameter(1, city);
+		List<String> query1 = query.getResultList();
+		close();
+		return query1;
 	}
+	
+	public List<String> getArrival(String city) {
+		
+		emf = Persistence.createEntityManagerFactory(DBName);
+		db = emf.createEntityManager();
+		System.out.println("Database ON");
+		
+		TypedQuery<String> query = db.createQuery("SELECT DISTINCT f.arrivingCity FROM Flight f WHERE f.departingCity=?1", String.class);
+		query.setParameter (1, city);
+		List<String> query1 = query.getResultList();
+		close();
+		return query1;
+	}
+	
+	
 
 }
